33b8b828722d3edea666ef76d28838bdb03d3666
Giulia A.D. Savorgnan <giulia.savorgnan@gmail.com>
2018-10-06 17:47:03 -0300

    fixed dictionary keys Error when loading DenseNet model

--- HeatmapGenerator.py
+++ HeatmapGenerator.py
@@ -36,8 +36,29 @@ class HeatmapGenerator ():
           
         model = torch.nn.DataParallel(model).cuda()
 
+        print("=> loading checkpoint")
         modelCheckpoint = torch.load(pathModel)
-        model.load_state_dict(modelCheckpoint['state_dict'])
+        # Error when loading DenseNet model : Missing key(s) in state_dict
+        # https://github.com/KaiyangZhou/deep-person-reid/issues/23
+        # The error is caused by the mismatch in keys, e.g. layers were named 'norm.1', 'conv.1', 
+        # but are now named 'norm1', 'conv1' (I trained the model with the old torchvision). 
+        # modify:
+        # '.'s are no longer allowed in module names, but pervious _DenseLayer
+        # has keys 'norm.1', 'relu.1', 'conv.1', 'norm.2', 'relu.2', 'conv.2'.
+        # They are also in the checkpoints in model_urls. 
+        # This pattern is used to find such keys.
+        # https://github.com/pytorch/vision/blob/50b2f910490a731c4cd50db5813b291860f02237/torchvision/models/densenet.py#L28
+        pattern = re.compile(
+            r'^(.*denselayer\d+\.(?:norm|relu|conv))\.((?:[12])\.(?:weight|bias|running_mean|running_var))$')
+        state_dict = modelCheckpoint['state_dict']
+        for key in list(state_dict.keys()):
+            res = pattern.match(key)
+            if res:
+                new_key = res.group(1) + res.group(2)
+                state_dict[new_key] = state_dict[key]
+                del state_dict[key]
+        model.load_state_dict(state_dict)
+        print("=> loaded checkpoint")
 
         self.model = model.module.densenet121.features
         self.model.eval()
